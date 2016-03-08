aaca2e40e2d84356ea0a1c59b631a9ce2b247f90
Antonio Nikolov <anikolovn@gmail.com>
2016-03-08 16:15:54 +0000

    Added current score and target score labels. Logic to do the scoring still to be implemented

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour
+{
+    public int _score = 0;
+    public string _scoreFormat = "Score: {0}";
+
+    private Text _scoreText;
+
+
+    void Awake()
+    {
+        _scoreText = GetComponent<Text>();
+    }
+
+
+    void Update()
+    {
+        _scoreText.text = string.Format(_scoreFormat, _score);
+    }
+}
